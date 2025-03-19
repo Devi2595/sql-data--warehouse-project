668c707f1d43cbc0a81ff2ef68cf182c348e8843
Devi2595 <srijanimoturi2595@gmail.com>
2025-03-19 11:21:39 -0700

    Update init_daatabase.sql

--- scripts/init_daatabase.sql
+++ scripts/init_daatabase.sql
@@ -1,6 +1,6 @@
 /*
 ==================================================================
-Create Databse and Schema in MySQl
+Create Database and Schema in MySQl
 ==================================================================
 Script purpose: 
   This script creates a new database named 'DataWarehouse' after checking if it already exists.
@@ -11,9 +11,23 @@ WARNING :
   All the data in the database will be permanently deleted. Proceed with caution and ensure you have proper backups before running this scripts.
 */
 
+USE master;
+GO
+  
 -----Drop and recreate the 'DataWarehouse' database
+/* for mysql
 DROP DATABASE IF EXISTS DataWarehouse;
 CREATE DATABASE DataWarehouse;
+*/
+--- for sql server
+IF EXISTS (SELECT 1 FROM sys.Databases WHERE name = 'DataWarehouse')
+BEGIN 
+  ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- create the 'DataWarehouse' database
 USE DataWarehouse;
 
 -----Create Schemas
